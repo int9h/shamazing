b2479708e7cf235fe566beb59834a671e5630d3f
Manuel Wildauer <m.wildauer@gmail.com>
2013-11-23 22:54:37 +0100

    ADD: list of strings

--- lib/Shamazing.py
+++ lib/Shamazing.py
@@ -1,9 +1,56 @@
 import re
 
+"""
+" @author Manuel Wildauer <m.wildauer@gmail.com>
+"
+"""
 class Shamazing:
+    
+    VERSION = "0.0.2"
 
+    """
+    " @param string string
+    " @return string
+    """
     def string(string):
-        return max(re.findall(r'[a-zA-Z]+', string), key=len)
+        strings = re.findall(r'[a-zA-Z]+', string) or False
+        if strings != False:
+            return max(strings, key=len)
 
+        return "No strings found"
+
+    """
+    " @param string string
+    " @return string
+    """
     def integer(string):
-        return max(re.findall(r'[0-9]+', string), key=len)
+        ints = re.findall(r'[0-9]+', string) or False
+        if ints != False:
+            return max(ints, key=len)
+
+        return "No integer found"
+    
+    """
+    " @param list listOfStrings
+    " @return string
+    """
+    def stringFromList(listOfStrings):
+        strings = list()
+        for sha in listOfStrings:
+            strings.append(Shamazing.string(sha))
+        
+        return max(strings, key=len)
+
+    """
+    " @param list listOfStrings
+    " @return string
+    """
+    def integerFromList(listOfStrings):
+        integers = list()
+        for sha in listOfStrings:
+            integers.append(Shamazing.integer(sha))
+
+        return max(integers, key=len)
+
+
+
